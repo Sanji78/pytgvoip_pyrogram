6764a31794d5f69af060dc3674d05e507c948e2d
Sanji78 <166992508+Sanji78@users.noreply.github.com>
2026-02-07 15:18:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def get_long_description():
 
 setup(
     name='stefano-pytgvoip-pyrogram',
-    version="0.0.14",
+    version="0.0.15",
     license='LGPLv3+',
     author='bakatrouble',
     author_email='bakatrouble@gmail.com',
@@ -56,7 +56,7 @@ setup(
     python_required='~=3.5',
     packages=['tgvoip_pyrogram'],
     install_requires=[
-        'stefano-pytgvoip>=0.0.7.67',
+        'stefano-pytgvoip>=0.0.7.6',
         'stefano-Pyrogram>=2.0.107'
     ],
     zip_safe=False,
